03c329d519a6629d0d7eb253b62b9956a972703b
minji0608 <106049821+minji0608@users.noreply.github.com>
2022-09-06 23:53:46 +0900

    feat: snap kit 기조단계

--- jointGoal_Swift/HomeViewController.swift
+++ jointGoal_Swift/HomeViewController.swift
@@ -15,18 +15,25 @@ class HomeViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        self.view.backgroundColor = .white
         self.view.addSubview(redView)
         self.view.addSubview(blueView)
         
         redView.backgroundColor = .red
         blueView.backgroundColor = .blue
+        
+        snapView()
     }
     
     func snapView() {
         redView.snp.makeConstraints { make in
             make.top.equalTo(view.snp.top)
-            make.size.width.height.equalTo(100)
+            
+            make.size.width.equalTo(400)
+            make.size.height.equalTo(500)
             make.left.equalTo(view.snp.left)
+            make.top.equalToSuperview().offset(10)
+        
         }
         
         blueView.snp.makeConstraints { make in
